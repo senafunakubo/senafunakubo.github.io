b194c8fe9896efdb0eacd9b81410a1d4927bd5e0
Sena <inabacker24@gmail.com>
2016-12-09 13:31:51 -0800

    changed
    changed many things again

--- js/po.js
+++ js/po.js
@@ -1,7 +1,15 @@
 $(function(){
+  
    $(".PhotoBox").hover(function(){
       $(".SlideBox",this).stop().animate({top:"0px"},"fast");
    },function(){
       $(".SlideBox",this).stop().animate({top:"200px"},"slow");
    });
+
+   $(".PhotoBox").hover(function(){
+      $(".SlideBox2",this).stop().animate({top:"0px"},"fast");
+   },function(){
+      $(".SlideBox2",this).stop().animate({top:"200px"},"slow");
+   });
+
 });
