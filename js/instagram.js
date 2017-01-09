e43fad66a1c0c9d6449b9393a22443df0d390567
Sena <inabacker24@gmail.com>
2017-01-09 12:43:47 -0800

    task_test

---
+++ js/instagram.js
@@ -0,0 +1,17 @@
+var token = '3586492222.9462142.5174a0635898455bae025ce22d526bc9',
+    username = 'sena17824';
+
+  $.ajax({
+    url: 'https://api.instagram.com/v1/users/self/?access_token='+ token,
+    type: 'GET',
+    dataType: 'jsonp',
+    success: function(ist){
+      // $("#myName").html(ist.data["full_name"]);
+      $('#loginArea').on('click', function() {
+        $("#myName").html(ist.data["full_name"]);
+        $("#loginArea").hide();
+        $("#logoutArea").show();
+      });
+    }
+
+  });
