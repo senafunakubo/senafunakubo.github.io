5cb63aba92bae723d33d80532a920a08a175ee1b
Sena <inabacker24@gmail.com>
2017-01-10 11:24:08 -0800

    modified

--- js/instagram.js
+++ js/instagram.js
@@ -22,8 +22,8 @@ var token = '3586492222.9462142.5174a0635898455bae025ce22d526bc9',
     dataType: 'jsonp',
     type: 'GET',
     success: function(data){
-     console.log(data[0][1]);
-     document.getElementById('userName') = data[0][1];
+     console.log(data[0]);
+     document.getElementById('userName') = data[0];
      //$("#userName").html(data[0]);
 
   }
