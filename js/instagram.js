9f3bb7ed1f753030486c5f526b9c61469514ebc7
Sena <inabacker24@gmail.com>
2017-01-10 11:18:03 -0800

    add

--- js/instagram.js
+++ js/instagram.js
@@ -6,7 +6,6 @@ var token = '3586492222.9462142.5174a0635898455bae025ce22d526bc9',
     type: 'GET',
     dataType: 'jsonp',
     success: function(ist){
-      // $("#myName").html(ist.data["full_name"]);
       $('#178mo').on('click', function() {
         $("#myName").html(ist.data["full_name"]);
         $("#loginArea").hide();
@@ -17,3 +16,15 @@ var token = '3586492222.9462142.5174a0635898455bae025ce22d526bc9',
   });
 
 //if else
+
+  $.ajax({
+    url: 'https://api.instagram.com/v1/users/self/follows?access_token=' + token,
+    dataType: 'jsonp',
+    type: 'GET',
+    success: function(){
+     console.log(data[0][1]);
+     document.getElementById('userName') = data[0][1];
+     //$("#userName").html(data[0]);
+
+  }
+});
