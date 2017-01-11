f3661d7e9a23718556213dff2571263485024988
Sena <inabacker24@gmail.com>
2017-01-11 11:50:32 -0800

    test

--- js/instagram.js
+++ js/instagram.js
@@ -10,25 +10,28 @@ if (hash.substr(0,14) == '#access_token=') {
         $("#myInfoPhoto").append("<img src=" + ist.data.profile_picture + ">");
           console.log(ist.data.profile_picture)
         $("#login").hide();
-        $("#userName").show();
         $("#logout").show();
 
         }
 
   });
 
-  $.ajax({
-    url: 'https://api.instagram.com/v1/users/self/follows?access_token=' + token,
-    dataType: 'jsonp',
-    type: 'GET',
-    success: function(data){
-     $("#userName").html(data);
-      for(let follower of data.data) {
-       $("#userName").append("<img src=" + follower.profile_picture + "><p>"+follower.full_name+"</p>");
-      }
-    }
-  });
+  // $.ajax({
+  //   url: 'https://api.instagram.com/v1/users/self/follows?access_token=' + token,
+  //   dataType: 'jsonp',
+  //   type: 'GET',
+  //   success: function(data){
+  //    $("#userName").html(data);
+  //     for(let follower of data.data) {
+  //      $("#userName").append("<img src=" + follower.profile_picture + "><p>"+follower.full_name+"</p>");
+  //     }
+  //   }
+  // });
 
 }else{
   console.log("failed");
+}
+
+function (){
+
 }
