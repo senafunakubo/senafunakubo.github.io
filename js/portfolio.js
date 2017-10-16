d19da8293d2afc0ca37da8d0fc9a82c93f3de7b7
Sena <inabacker24@gmail.com>
2017-10-15 22:35:57 -0700

    Added popup

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,7 +1,34 @@
+// $(function(){
+//
+//   $('.button').mouseover(function () {
+//     $(this).fadeOut("slow");
+//   });
+//
+// });
+
+(function($){
 $(function(){
+    $(document)
+        .on('click', '.popup_btn', function(){
+            var $popup = $((this).attr('href'));
 
-  $('.button').mouseover(function () {
-    $(this).fadeOut("slow");
-  });
+            // ポップアップの幅と高さからmarginを計算する
+            var mT = ($popup.outerHeight() / 2) * (-1) + 'px';
+            var mL = ($popup.outerWidth() / 2) * (-1) + 'px';
 
+            // marginを設定して表示
+            $('.popup').hide();
+            $popup.css({
+                'margin-top': mT,
+                'margin-left': mL
+            }).show();
+            $('#overlay').show();
+
+            return false;
+        })
+        .on('click', '.close_btn, #overlay', function(){
+            $('.popup, #overlay').hide();
+            return false;
+        });
 });
+})(jQuery);
