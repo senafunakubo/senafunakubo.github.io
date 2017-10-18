de743fd116b3dcf1a33d33ac750caf91824a1bde
Sena <inabacker24@gmail.com>
2017-10-18 00:09:27 -0700

    deleted pics, modified a size of flyer and added a sentence

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,48 +1,48 @@
-(function($){
-$(function(){
-
-  // $('.button').click(function () {
-  //   if(!navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/)){
-  //       $('html, body').css({
-  //         overflow: 'hidden',
-  //         height: '100%'
-  //       });
-  //   }
-  // });
-
-  var position = $(".skills").offset().top;
-
-    $(document)
-        .on('click', '.popup_btn', function(){
-            var $popup = $($(this).attr('href'));
-
-            $('.popup').hide();
-            $popup.css({}).show();
-            $('#overlay').show();
-
-            if(navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/)){
-              $('html, body, #popup1').css({
-                height: 'auto'
-              });
-            }
-
-            return false;
-        })
-        .on('click', '.close_btn, #overlay', function(){
-            $('.popup, #overlay').hide();
-            if(!navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/)){
-                $('html, body').css({
-                  overflow: 'auto',
-                  height: 'auto'
-                });
-            }
-
-            $(window).scrollTop(position);
-
-            return false;
-        });
-
-});
-
-
-})(jQuery);
+// (function($){
+// $(function(){
+//
+//   $('.button').click(function () {
+//     if(!navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/)){
+//         $('html, body').css({
+//           overflow: 'hidden',
+//           height: '100%'
+//         });
+//     }
+//   });
+//
+//   var position = $(".skills").offset().top;
+//
+//     $(document)
+//         .on('click', '.popup_btn', function(){
+//             var $popup = $($(this).attr('href'));
+//
+//             $('.popup').hide();
+//             $popup.css({}).show();
+//             $('#overlay').show();
+//
+//             if(navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/)){
+//               $('html, body, #popup1').css({
+//                 height: 'auto'
+//               });
+//             }
+//
+//             return false;
+//         })
+//         .on('click', '.close_btn, #overlay', function(){
+//             $('.popup, #overlay').hide();
+//             if(!navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/)){
+//                 $('html, body').css({
+//                   overflow: 'auto',
+//                   height: 'auto'
+//                 });
+//             }
+//
+//             $(window).scrollTop(position);
+//
+//             return false;
+//         });
+//
+// });
+//
+//
+// })(jQuery);
