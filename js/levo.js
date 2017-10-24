5403e62fdfe21b11f0ac3e88e658ffc053fb4f54
Sena <inabacker24@gmail.com>
2017-10-23 20:53:06 -0700

    Modified a service part

---
+++ js/levo.js
@@ -0,0 +1,10 @@
+// $(function() {
+//     var triggerNode = $(".service-description");
+//
+//     $(window).scroll(function(){
+//         var triggerNodePosition = $(triggerNode).offset().top - $(window).height();
+//         if ($(window).scrollTop() > triggerNodePosition) {
+//             $('h2').css('color','#4A9452');
+//         }
+//     });
+//   });
