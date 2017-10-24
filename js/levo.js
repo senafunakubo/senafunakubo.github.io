bbdac938c57755af2679110bb14fb3b50de3c50a
Sena <inabacker24@gmail.com>
2017-10-24 05:13:57 -0700

    changed a colour by JS, font

--- js/levo.js
+++ js/levo.js
@@ -1,10 +1,11 @@
-// $(function() {
-//     var triggerNode = $(".service-description");
-//
-//     $(window).scroll(function(){
-//         var triggerNodePosition = $(triggerNode).offset().top - $(window).height();
-//         if ($(window).scrollTop() > triggerNodePosition) {
-//             $('h2').css('color','#4A9452');
-//         }
-//     });
-//   });
+$(function() {
+    var triggerNode = $(".service-description");
+
+    $(window).scroll(function(){
+        var triggerNodePosition = $(triggerNode).offset().top - $(window).height();
+        if ($(window).scrollTop() > triggerNodePosition) {
+            $('h2').css('color','#B48B5B');
+        }
+    });
+
+  });
